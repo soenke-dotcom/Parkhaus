0915d5f9fb00f4fef1bbcd388d8117ce483afb4f
Sönke Adam <soenkeadam@gmail.com>
2024-05-31 16:10:07 +0200

    läuft, aber Parkhaus überfüllt

--- library.c
+++ library.c
@@ -1,48 +1,65 @@
-#include "library.h"
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+
 #define CARS 500
 
 pthread_t pts[CARS];
 pthread_t out;
-int slots [500];
+int slots [100];
 int currentCars = 0;
+int allCarsDone = 1;
 
 //AusgabeThread
 void * ausgabe() {
+
     //Beendet wenn jedes Autothread beednet
-    while(1) {
-        printf("Belegte Plätze: &d\n Freie Plätze: &d\n", currentCars, CARS - currentCars);
+    while(allCarsDone) {
+        printf("Belegte Plätze: %d \n Freie Plätze: %d \n", currentCars, 100 - currentCars);
         sleep(1);
     }
-
+    pthread_exit(NULL);
 }
 
 //AutoThread
 void * car() {
+
+    //Parkplatz Nummer finden
     int i = 0;
-    while ()
-    while (currentCars < CARS) {
+    while (pthread_self() != pts[i]) {
+        i++;
+    }
+    //warten falls voll
+    while(currentCars >= 100) {
 
     }
+    //slots[i / 5] = 1;
+    currentCars++;
+    sleep((rand() % 4) + 1);
+    //slots[i / 5] = 0;
+    currentCars--;
+
+    pthread_exit(NULL);
 }
 
 int main() {
 
     //AusgabeThread erzeugen
-    pthread_create(out, NULL, ausgabe, NULL);
+    pthread_create(&out, NULL, ausgabe, NULL);
 
     //AutoThread erzeugen
     for (int i = 0; i < CARS; i++) {
 
         pthread_create(&pts[i], NULL, car, NULL);
     }
-    //Variable setzen --> Ausgabe Thread beenden
-
     //AufAuto Threads warten
     for (int i = 0; i < CARS; i++) {
 
-        pthread_join(&pts[i], NULL);
+        pthread_join(pts[i], NULL);
     }
-
+    allCarsDone = 0;
 }
